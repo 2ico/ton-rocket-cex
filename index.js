36728e79e65aaf59512b2ad4a0f98a44c035bbb0
2ico <duico+heroku@pm.me>
2022-09-28 22:38:48 +0200

    Minimal index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+import { Telegraf } from "telegraf";
+const DOMAIN = "desolate-savannah-32183.herokuapp.com"
+const PORT = 5000
+
+const bot = new Telegraf(token);
+
+bot.on("text", ctx => ctx.reply("Hello"));
+
+// Start webhook via launch method (preferred)
+bot.launch({
+  webhook: {
+    // Public domain for webhook; e.g.: example.com
+    domain: DOMAIN,
+
+    // Port to listen on; e.g.: 8080
+    port: PORT,
+
+    // Optional path to listen for.
+    // `bot.secretPathComponent()` will be used by default
+    hookPath: '',
+    
+    // Optional secret to be sent back in a header for security.
+    // e.g.: `crypto.randomBytes(64).toString("hex")`
+    secretToken: crypto.randomBytes(64).toString("hex"),
+  },
+});
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
