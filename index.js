ab5de64fc9bb55d13784df7d881b84d3a497daa3
2ico <duico+heroku@pm.me>
2022-09-28 23:01:23 +0200

    Do not log env

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const DOMAIN = "desolate-savannah-32183.herokuapp.com"
 const PORT = 5000
 
 require('dotenv').config()
-console.log(process.env)
 
 const bot = new Telegraf(process.env.BOT_API_TOKEN);
 
