23455c1af451bdd0b3900ebf2bb83a74761c2122
2ico <duico+heroku@pm.me>
2022-10-01 16:17:39 +0200

    add react-telegram-webapp

--- ton-rocket-cex-webapp/src/App.tsx
+++ ton-rocket-cex-webapp/src/App.tsx
@@ -1,15 +1,42 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import './App.css'
+// import { useState } from 'react'
+// // import reactLogo from './assets/react.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       Test React app !!
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useState } from 'react';
+import { withTelegramWebApp } from 'react-telegram-webapp';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      Test React app !!
-    </div>
-  )
+    return (
+        <div className="App">
+            Test React app !!
+        </div>
+    )
 }
 
-export default App
+async function validateHash(hash: string) {
+    const response = await fetch(`/api/validate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ hash }),
+    });
+
+    return response.ok;
+}
+
+export default withTelegramWebApp(App, {
+    validateHash
+});
