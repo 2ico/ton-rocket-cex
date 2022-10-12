9333526af052d23186649fd197291b49811d4198
2ico <duico+heroku@pm.me>
2022-10-12 17:29:15 +0200

    Reduce MuiAppBar boxShadow

--- ton-rocket-cex-webapp/src/theme/theme.ts
+++ ton-rocket-cex-webapp/src/theme/theme.ts
@@ -27,7 +27,8 @@ declare module '@mui/material/styles' {
       MuiAppBar: {
         styleOverrides: {
           root: {
-            backgroundColor: "var(--tg-theme-bg-color)"
+            backgroundColor: "var(--tg-theme-bg-color)",
+            boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2)" // 0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)"
           }
         }
       },
