c8cdf4244a1cde032775fa6717886a162af01c81
2ico <duico+heroku@pm.me>
2022-10-14 05:44:03 +0200

    Improve OrderItem flex

--- ton-rocket-cex-webapp/src/components/OrderItem.tsx
+++ ton-rocket-cex-webapp/src/components/OrderItem.tsx
@@ -16,23 +16,23 @@ const OrderItem = ({ order, onCancel } : OrderItemProp)
 {
     return (
         <Box>
-            <ListItem 
-                sx={{ width: "100%", pl: 4 }}
+            <ListItem
+                sx={{ width: "100%", pl: 4, flexWrap: 'wrap' }}
                 secondaryAction = {
                     <IconButton color="primary" onClick={() => onCancel(order.id)}>
                         <Cancel />
                     </IconButton>
                 }
             >
-                <Box flexBasis={"32px"} sx={{ color: (order.orderAction == OrderAction.Buy ? "rgb(49, 181, 69)" : "#FF4C4C")}}>
+                <Box flexBasis={"32px"} sx={{ pr: 2, color: (order.orderAction == OrderAction.Buy ? "rgb(49, 181, 69)" : "#FF4C4C")}}>
                 <ListItemIcon sx={{minWidth: "0px"}}>
                     { order.orderAction == OrderAction.Buy ? <TrendingUp sx={{color: "rgb(49, 181, 69)"}}/> : <TrendingDown sx={{color: "#FF4C4C"}}/> }
                 </ListItemIcon>
                 <Typography color="inherit">{order.orderAction}</Typography>
                 </Box>
-                <ListItemText sx={{flexBasis: "120px"}} primary={order.pair.base_name+"/"+order.pair.quote_name} secondary={order.orderType}/>
-                <ListItemText sx={{flexBasis: "80px"}} primary={<React.Fragment>{order.price.toNumber().toPrecision(5)}</React.Fragment>}/>
-                <ListItemText sx={{flexBasis: "120px"}} primary={<React.Fragment>{order.amount.toNumber().toPrecision(5)} {order.pair.base_name}</React.Fragment>}/>
+                <ListItemText sx={{flexBasis: "72px"}} primary={order.pair.base_name+"/"+order.pair.quote_name}/>
+                <ListItemText sx={{flexBasis: "48px"}} primary={<React.Fragment>{order.price.toNumber().toPrecision(5)}</React.Fragment>} secondary={order.orderType}/>
+                <ListItemText sx={{flexBasis: "60px"}} primary={<React.Fragment>{order.amount.toNumber().toPrecision(5)} {order.pair.base_name}</React.Fragment>}/>
             </ListItem>
         </Box>
     )
